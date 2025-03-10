d95830c3243e99bcc46005dad1a2038a82dced67
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2025-03-10 10:55:23 +0000

    Initial Frontend Commit - tailwind + react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1
+        className="text-2xl text-center mt-10 text-blue-500"
+      >
+        Manas is a man of culture <br />
+        Kaushal is a man of action <br />
+        Parth is a man of word
+      </h1>
+    </>
+  )
+}
+
+export default App
