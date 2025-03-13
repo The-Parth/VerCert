84c64cef74aa7fe4d410c6413ee58bba75283ca5
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2025-03-13 13:21:03 +0000

    frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// get environment variables
+import dotenv from 'dotenv';
+dotenv.config();
+
+const HOSTED = process.env.HOSTED || false;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: HOSTED ? '/frontend/' : '/',
   server: {
     port: 5000, // You can change this if needed
   },
