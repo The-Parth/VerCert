c1d6db35a7bbebc1f1eba2943128d07de8fc6cfd
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2025-04-04 16:02:05 +0100

    Locked in Hardhat for Testing and Hoodi Deploys

---
+++ solidity/hardhat/hardhat.config.js
@@ -0,0 +1,17 @@
+require('@openzeppelin/hardhat-upgrades');
+require('@nomicfoundation/hardhat-toolbox');
+require("./tasks/fundwallet");
+require('dotenv').config();
+
+module.exports = {
+  solidity: '0.8.26',
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+    },
+    hoodi: {
+      url: process.env.HOODI_RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+};
